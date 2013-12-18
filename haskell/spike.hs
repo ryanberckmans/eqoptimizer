9f22f1a3d9f1e995827dc51bd962d863a30912fb
Ryan Berckmans <ryan.berckmans@gmail.com>
2013-12-18 03:49:44 -0500

    add trial #2 to haskell/spike.hs

--- haskell/spike.hs
+++ haskell/spike.hs
@@ -1,3 +1,5 @@
+import Data.List
+import Data.Ord
 
 {-
 	eqoptimizer trial #1
@@ -26,5 +28,35 @@ eqoptimizer1 = do
 	putStrLn $ "optimized_set: " ++ show (optimize_eq possible_eq)
 	putStrLn $ "eq_stat: " ++ show (eq_stat(optimize_eq possible_eq))
 
+{-
+	eqoptimizer trial #2
+
+	As in #1, but equipment is now a data type with a name and hp amount
+-}
+
+data Item1 = Item1 {
+	itemName :: String,
+	itemHp :: Int
+	} deriving (Show)
+
+possible_eq2 :: [[Item1]]
+possible_eq2 = [
+	[Item1 "Diamond orb of Tyche" 45, Item1 "Pulsating Ruby" 30],
+	[Item1 "A bracelet of strange silvery mist" 37, Item1 "A blue-steel bracer from bloodstone" 44]]
+
+optimize_eq2 :: [[Item1]] -> [Item1]
+optimize_eq2 eqs = [maximumBy (comparing itemHp) eq | eq <- eqs]
+
+set_hp :: [Item1] -> Int
+set_hp eqs = sum [itemHp eq | eq <- eqs]
+
+eqoptimizer2 = do
+	putStrLn "eqoptimizer trial #2"
+	putStrLn $ "possible_eq: " ++ show possible_eq2
+	let optimized_eq = optimize_eq2 possible_eq2
+	putStrLn $ "optimized_set: " ++ show optimized_eq
+	putStrLn $ "set_hp: " ++ show (set_hp optimized_eq)
+
 main = do
 	eqoptimizer1
+	eqoptimizer2
