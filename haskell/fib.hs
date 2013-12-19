a8a76be4bced7f116335892d500cde86f716f918
Ryan Berckmans <ryan.berckmans@gmail.com>
2013-12-19 03:05:33 -0500

    add haskell/fib.hs, some fibonnaci examples to help learn haskell

---
+++ haskell/fib.hs
@@ -0,0 +1,13 @@
+
+
+fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+fibs2 = 0 : 1 : [ a+b | a <- fibs2 | b <- tail fibs2 ]
+fibs3 = 0 : scanl (+) 1 fibs3
+ 
+
+{-  fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+	fibs = 0 : 1 : ((+) 0 1 : zipWith (+) (tail fibs) (tail (tail fibs)))
+	fibs = 0 : 1 : 1 : ((+) 1 1 : zipWith (+) (tail (tail fibs)) (taii (tail (tail fibs)))))    
+	fibs = 0 : 1 : 1 : 2 : ((+) 1 2 : zipWith (+) (tail (tail (tail fibs))) (tail (taii (tail (tail fibs))))))
+	fibs = 0 : 1 : 1 : 2 : 3 : ((+) 2 3 : zipWith (+) (tail (tail (tail (tail fibs)))) (tail (tail (taii (tail (tail fibs)))))))
+-}
